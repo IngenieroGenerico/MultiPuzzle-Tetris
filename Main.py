f67361f452c8a422cc5382066aca2fcea00a0abf
Die616 <die_616@hotmail.com>
2024-01-16 23:16:44 -0600

    Windows manager

--- Main.py
+++ Main.py
@@ -5,32 +5,23 @@ from src.Resources.AudioManager import AudioManager
 from src.Resources.InputManager import InputManager
 
 if __name__ == "__main__":
-    audio_manager = AudioManager()
-    input_manager = InputManager()
     new_grid = Grid()
 
     areas_amount = 3
     rows_amount = 22
     columns_amount = 12
-
     block_size = 20
     height_gameplay_area = rows_amount * block_size
     width_gameplay_area = areas_amount * columns_amount * block_size
 
     new_grid.create_level(areas_amount,columns_amount,rows_amount)
 
-    audio_manager.load_music("Test", "src/Resources/Music/gameplay.mp3")
-    audio_manager.set_music_volume(0.1)
-    audio_manager.play_music("Test")
-
-    audio_manager.load_sound("Key", "src/Resources/Music/Test_key_pressed.mp3")
-
     game = EventManager(width_gameplay_area, height_gameplay_area)
     render = RenderManager(game.get_screen())
     
     
     while game.is_running():
-        game.hande_input(new_grid)
+        game.handle_input(new_grid)
         game.update_game(new_grid, render)
     game.destroy()
 
--- src/Resources/EventManager.py
+++ src/Resources/EventManager.py
@@ -1,63 +1,36 @@
 import pygame
+from ..Grid import Grid
 from .InputManager import InputManager
 from .AudioManager import AudioManager
+from .WindowsManager import WindowsManager
+from .RenderManager import RenderManager
 
 class EventManager:
     "__privada, _Protected"
     def __init__(self, width: int, height: int, window_name: str = "My Game") -> None:
-        pygame.init() #TODO: Esto va en el GameManager.
-        self.__width_gampley_area = width
-        self.__height_gameplay_area = height
-
-        self.__width_rules_area = self.__width_gampley_area + 100
-        self.__height_rules_area = 80
-
-        self.__width_score_area = 100
-        self.__height_score_area = self.__height_gameplay_area
-
-        self.__screen = pygame.display.set_mode((self.__width_gampley_area + self.__width_score_area, self.__height_gameplay_area + self.__height_rules_area))
-        pygame.display.set_caption(window_name)
-
-
-        # Definir colores
-        blanco = (255, 255, 255)
-        rojo = (255, 0, 0)
-        verde = (0, 255, 0)
-        # Crear superficie para el área de puntos y siguiente pieza
-        self.__score_area = pygame.Surface((self.__width_score_area, self.__height_score_area))
-        self.__score_area.fill(blanco) #TODO: score, next piece, etc.
-
-        # Crear superficie para el de reglas
-        self.__rules_area = pygame.Surface((self.__width_rules_area, self.__height_rules_area))
-        self.__rules_area.fill(verde)#TODO: Imagen
-
-        #TODO:Crear superficie para el area de juegos.
-        
-        self.__running = True
-        self.__clock = pygame.time.Clock()
+        self.__windows_manager = WindowsManager(width, height, window_name)
         self.__input_manager = InputManager()
         self.__audio_manager = AudioManager()
-
-    #Crear input manager
-    #Crear audio manager
-    def get_screen(self) -> pygame.Surface:
-        return self.__screen
+        self.__clock = pygame.time.Clock()
+        self.__running = True
+    def get_screen(self):
+        return self.__windows_manager.get_screen()
     
-    def hande_input(self, new_grid) -> None:
+    def handle_input(self, new_grid: Grid) -> None:
         self.__input_manager.update()
         if self.__input_manager.key_pressed("W"):
             print("W pressed")
         if self.__input_manager.key_pressed("A"):
             new_grid.get_actual_piece().move_left()
-            #self.__audio_manager.play_sound("Key")
+            self.__audio_manager.play_sound("Key")
             self.__input_manager.keys["A"] = False
         if self.__input_manager.key_pressed("S"):
             new_grid.get_actual_piece().move_down()
-            #self.__audio_manager.play_sound("Key")
+            self.__audio_manager.play_sound("Key")
             self.__input_manager.keys["S"] = False
         if self.__input_manager.key_pressed("D"):
             new_grid.get_actual_piece().move_right()
-            #self.__audio_manager.play_sound("Key")
+            self.__audio_manager.play_sound("Key")
             self.__input_manager.keys["D"] = False
         if self.__input_manager.key_pressed("UP"):
             print("UP pressed")
@@ -72,19 +45,19 @@ class EventManager:
         if self.__input_manager.key_pressed("ESC"):
             print("ESC pressed")
 
-
-    def update_game(self, grid, render) -> None:
+    def update_game(self, grid: Grid, render: RenderManager) -> None:
         """
         Starts the main game loop
         """
         render.clear_screen()
 
-         # Renderizar el score_area 
-        self.__screen.blit(self.__score_area, (self.__width_gampley_area, 0))
+        # Renderizar el score_area
+        #self.__windows_manager.get_screen().blit(self.__windows_manager.get_score_area(), 
+        #                                         (self.__windows_manager.get_width_gameplay_area(), 0))
 
         # Renderizar el rules_area 
-        self.__screen.blit(self.__rules_area, (0, self.__height_gameplay_area))
-
+        #self.__windows_manager.get_screen().blit(self.__windows_manager.get_rules_area(), 
+        #                                         (0, self.__windows_manager.get_height_gameplay_area()))
         
         grid.render(render)
         render.update_display()
