1fd6c3bab71ccaf4e76471bef9134c9065c18d0a
Die616 <die_616@hotmail.com>
2024-01-23 18:20:11 -0600

    input manager

--- src/Resources/InputManager.py
+++ src/Resources/InputManager.py
@@ -2,23 +2,15 @@ import pygame
 
 class InputManager:
     def __init__(self) -> None:
-        """
-        Diccionario para almacenar el estado de todas las teclas 
-        """
+        self.keys = set()
         self.mouse_state = None
-        self.keys = {
-            "W": False,
-            "A": False,
-            "S": False,
-            "D": False,
-            "UP": False,
-            "DOWN": False,
-            "LEFT": False,
-            "RIGHT": False,
-            "ESC": False,
-            "TAB": False
-        }
-    
+
+    def set_key_states(self, key, state) -> None:
+        if state:
+            self.keys.add(key)
+        else:
+            self.keys.discard(key)
+
     def mouse_position(self) -> tuple:
         mouse_x, mouse_y = pygame.mouse.get_pos()
         return mouse_x, mouse_y
@@ -38,46 +30,17 @@ class InputManager:
             if event.type == pygame.QUIT:
                 pygame.quit()
             elif event.type == pygame.KEYDOWN:
-                self.handle_key_down(event.key)
+                self.set_key_states(event.key, True)
+                print(self.key_pressed(pygame.K_w))
+                self.clear_keys()
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                self.mouse_pressed()
+
                 
-    def handle_key_down(self, key) -> None:
-        if key == pygame.K_w:
-            self.keys["W"] = True
-        elif key == pygame.K_a:
-            self.keys["A"] = True
-        elif key == pygame.K_s:
-            self.keys["S"] = True
-        elif key == pygame.K_d:
-            self.keys["D"] = True
-        elif key == pygame.K_UP:
-            self.keys["UP"] = True
-        elif key == pygame.K_DOWN:
-            self.keys["DOWN"] = True
-        elif key == pygame.K_LEFT:
-            self.keys["LEFT"] = True
-        elif key == pygame.K_RIGHT:
-            self.keys["RIGHT"] = True
-        elif key == pygame.K_TAB:
-            self.keys["TAB"] = True
-        elif key == pygame.K_ESCAPE:
-            self.keys["ESC"]  = True
-    
+                
     def clear_keys(self) -> None:
-        for key in self.keys:
-            self.keys[key] = False
+        self.keys.clear()
 
     def key_pressed(self, key: int) -> bool:
-        """
-        Verifica si una tecla esta siendo presionada
-
-        Args:
-            key (int): Tecla de verificacion
-
-        Returns:
-            bool: True si la tecla esta siendo presionada,
-                  False de lo contrario
-        """
-        if key in self.keys:
-            return self.keys[key]
-        return False
+        return key in self.keys
     
--- src/Resources/__init__.py
+++ src/Resources/__init__.py
@@ -1,5 +1,4 @@
 from .AudioManager import AudioManager
-from .EventManager import EventManager
 from .InputManager import InputManager
 from .ResourceManager import CImage, CSurface
 from .WindowsManager import WindowsManager
